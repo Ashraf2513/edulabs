c7e79c1375d67d2a9aaac329cf53c2662d773b86
Ashraf <shaikashraf33@gmail.com>
2021-10-23 22:26:01 +0530

    Added browser stack account

---
+++ Test Suites/Demo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a00447a2-2650-4127-96d5-23958af341f7</testSuiteGuid>
+   <testCaseLink>
+      <guid>d58fd4bb-8312-4297-965a-0f7928d9c0df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
